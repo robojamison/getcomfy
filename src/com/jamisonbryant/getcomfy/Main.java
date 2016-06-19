4d8701551a26b0499c4816804a52ae936799c351
Jamison Bryant <robojamison@gmail.com>
2016-06-18 21:17:13 -0400

    Create main class with main() and Hello World code

---
+++ src/com/jamisonbryant/getcomfy/Main.java
@@ -0,0 +1,17 @@
+package com.jamisonbryant.getcomfy;
+
+/**
+ *
+ */
+public class Main
+{
+    /**
+     * Application entry point. Processes command-line arguments and bootstraps application.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
